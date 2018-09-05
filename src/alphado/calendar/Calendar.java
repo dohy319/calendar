66d76ae788c432a49bf3a4e5d243909a0e108b56
Do <Do@DESKTOP-6C6IIOQ>
2018-09-05 19:54:13 +0900

    특정달 최대일수 출력하기

--- src/alphado/calendar/Calendar.java
+++ src/alphado/calendar/Calendar.java
@@ -3,6 +3,12 @@ package alphado.calendar;
 import java.util.Scanner;
 
 public class Calendar {
+	
+	private static final int[] day= {31,28,31,30,31,30,31,31,30,31,30,31};
+	
+	public int getMaxDaysofMonth(int month) {
+		return day[month-1];
+	}
 	public static void main(String[] args) {
 
 		System.out.println(" 일   월   화    수    목    금    토");
@@ -14,11 +20,13 @@ public class Calendar {
 		
 		// 숫자를 입력받아 해당하는 달의 일수 출력하기.
 		Scanner sc = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
 		int month = sc.nextInt();
-	
-		int []day = {31,28,31,30,31,30,31,31,30,31,30,31};
-		System.out.println(month+"월은 "+day[month-1]+"일까지 있습니다.");
+		
+	//	System.out.printf("%d월은 %d일까지 있습니다.\n",month, cal.getMaxDaysofMonth(month));
+		
+		System.out.println(month+"월은 "+cal.getMaxDaysofMonth(month)+"일까지 있습니다.");
 		
 	}
 }
